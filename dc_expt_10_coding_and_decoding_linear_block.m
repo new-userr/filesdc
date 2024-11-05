e21357448397f10e4be1e442014ea2392becbf7e
new-userr <168989422+new-userr@users.noreply.github.com>
2024-11-05 19:23:46 +0530

    Update dc_expt_10_coding_and_decoding_linear_block.m

--- dc_expt_10_coding_and_decoding_linear_block.m
+++ dc_expt_10_coding_and_decoding_linear_block.m
@@ -1,60 +1,69 @@
-% dc_expt_10_coding_and_decoding_linear_block_codes
 % Given H Matrix
-H= [1 1 1 0 1 0 0;
-0 1 1 1 0 1 0;
-1 1 0 1 0 0 1];
-k=4;
-n=7;
-%Generating G Matrix
-%taking the H matrix transpose
-P=H';
-%making a copy of H transpose matrix
-L=P;
-%taking the last 4 rows of L and storing
-L((5:7),:)=[];
-%creating a identity matrix of size k*k
-I=eye(k);
-%making a 4*7 matrix
-G=[I L];
-%generate U data vector, denoting all information sequences
-no=2^k;
-%iterate through an unit-spaced vector
-for i=1:2^k
-%iterate through vector with specified increment
-%or in simple words here we are decrementing 4 till we get 1
-for j=k:-1:1
-if rem(i-1,2^(-j + k + 1)) >= 2^(-j +k)
-u(i,j)=1;
-else
-u(i,j)=0;
+H = [1 1 1 0 1 0 0;
+     0 1 1 1 0 1 0;
+     1 1 0 1 0 0 1];
+k = 4; % Number of information bits
+n = 7; % Codeword length
+
+% Generating G Matrix
+P = H';         % Transpose of H matrix
+L = P;          % Copy of P matrix
+L((5:7), :) = []; % Remove the last 3 rows to get a 4x3 matrix
+I = eye(k);     % Identity matrix of size 4x4
+G = [I L];      % Construct G matrix as [I | L]
+
+% Generate U data vector, denoting all information sequences
+no = 2^k; % Total number of information sequences
+u = zeros(no, k); % Pre-allocate u matrix for efficiency
+
+for i = 1:no
+    for j = k:-1:1
+        if rem(i-1, 2^(-j + k + 1)) >= 2^(-j + k)
+            u(i, j) = 1;
+        else
+            u(i, j) = 0;
+        end
+    end
 end
-%to avoid displaying each iteration/loop value
-echo off;
+disp('Information sequences (u):');
+disp(u);
+
+% Generate codewords
+c = rem(u * G, 2);
+disp('Codewords (c):');
+disp(c);
+
+% Find the minimum Hamming distance
+w_min = min(sum(c(2:no, :), 2)); % Exclude the all-zero codeword
+disp('Minimum Hamming distance (w_min):');
+disp(w_min);
+
+% Given received codeword
+r = [0 0 0 1 0 0 0];
+disp('Received codeword (r):');
+disp(r);
+
+% Syndrome calculation
+ht = H';  % Transpose of H for syndrome decoding
+s = rem(r * ht, 2);
+disp('Syndrome (s):');
+disp(s);
+
+% Error correction
+for i = 1:size(ht, 1)
+    if all(ht(i, 1:3) == s)
+        r(i) = 1 - r(i); % Flip the bit at error position
+        break;
+    end
 end
-end
-echo on;
-u;
-%generate codewords
-c=rem(u*G,2);
-%find the min distance
-w_min='min(sum((c(2:2^k,:))))';
-%given recieved codeword
-r=[0 0 0 1 0 0 0];
-r;
-p=[G(:,n-k+2:n)];
-%find syndrome
-ht=transpose(H);
-s=rem(r*ht,2);
-for i=1 : 1 : size(ht)
-if(ht(i,1:3)==s)
-r(i)=1-r(i);
-break;
-end
-end
-disp('the error is in bit:')
-disp(i)
-disp('the corrected codeword is:')
-disp(r)
+
+disp('The error is in bit:');
+disp(i);
+disp('The corrected codeword is:');
+disp(r);
+
+
+
 
 %{ 
 1) H
